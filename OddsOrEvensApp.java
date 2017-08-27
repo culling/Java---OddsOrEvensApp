8d135d8414fa8320e4d9ca0634528c00cb4fbac7
culling <culling66@gmail.com>
2017-08-28 07:03:44 +0800

    initial commit

---
+++ OddsOrEvensApp.java
@@ -0,0 +1,19 @@
+/**
+* The TripPlannerApp is to resolve the edx Microsoft Java Odds or Evens project
+* https://courses.edx.org/courses/course-v1:Microsoft+DEV276x+2T2017
+*
+ */
+import java.util.Scanner;
+
+class OddsOrEvensApp{
+    public static void main(String[] args) {
+
+        part1();
+        
+    }
+
+    public static void part1(){
+        System.out.println("Part1");
+    }
+
+}
